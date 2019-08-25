a200befe2df200b24cfa98cd0bc430430cc236c1
JooYoung <sana2dang@naver.com>
2019-08-25 22:15:49 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo cp ./* /opt/retropie/supplementary/attractmode/sfml/lib/
 
-cp ./gpi_h_Fly9p_Theme/layout.nut /opt/retropie/configs/all/attractmode/layouts/gpi_h_Fly9p_Theme/layout.nut
-cp ./gpi_v_Fly9p_Theme/layout.nut /opt/retropie/configs/all/attractmode/layouts/gpi_v_Fly9p_Theme/layout.nut
-cp ./gpi_LEFTRIGHT_Menu/layout.nut /opt/retropie/configs/all/attractmode/layouts/gpi_LEFTRIGHT_Menu/layout.nut
+cp -r ./gpi_h_Fly9p_Theme/layout.nut /opt/retropie/configs/all/attractmode/layouts/gpi_h_Fly9p_Theme/layout.nut
+cp -r ./gpi_v_Fly9p_Theme/layout.nut /opt/retropie/configs/all/attractmode/layouts/gpi_v_Fly9p_Theme/layout.nut
+cp -r ./gpi_LEFTRIGHT_Menu/layout.nut /opt/retropie/configs/all/attractmode/layouts/gpi_LEFTRIGHT_Menu/layout.nut
